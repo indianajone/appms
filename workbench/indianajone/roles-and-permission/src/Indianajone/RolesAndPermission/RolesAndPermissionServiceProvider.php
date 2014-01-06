ae34f72adf0cdc82d5b50e39937ce2d55134c50e
Best <mmer555@hotmail.com>
2014-01-06 15:38:04 +0700

    Initial Role and Permission

---
+++ workbench/indianajone/roles-and-permission/src/Indianajone/RolesAndPermission/RolesAndPermissionServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Indianajone\RolesAndPermission;
+
+use Illuminate\Support\ServiceProvider;
+
+class RolesAndPermissionServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('indianajone/roles-and-permission');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
