653ab975efbbb87d28b75a289445071532c0ec78
Best <mmer555@hotmail.com>
2014-01-13 17:30:45 +0700

    Add todo list in routes

--- workbench/indianajone/roles-and-permissions/src/routes.php
+++ workbench/indianajone/roles-and-permissions/src/routes.php
@@ -2,11 +2,11 @@
 
 	Route::group(array('prefix' => 'api/v1'), function() {
 		/**
-		
-			TODO:
-			- Need Appkey filter.
-			- Need Get fields function.
-		
+		*
+		*	TODO:
+		*	- Need Appkey filter.
+		*	- Need Get fields function.
+		*
 		**/
 		
 		Route::post('roles/{id}/delete', 'Indianajone\\RolesAndsPermissions\\Controllers\\RoleController@delete');
