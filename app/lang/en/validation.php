5b24b2f63dbdf8738a41dbf2a7a1f3f52b85928a
Best <mmer555@hotmail.com>
2014-01-21 12:17:39 +0700

    Add Custom validation rule messages

--- app/lang/en/validation.php
+++ app/lang/en/validation.php
@@ -35,7 +35,10 @@ return array(
 	"digits_between"   => "The :attribute must be between :min and :max digits.",
 	"email"            => "The :attribute format is invalid.",
 	"exists"           => "The selected :attribute is invalid.",
-	// "existloop"        => "The selected :attribute is invalid.",
+	// Customs
+	"existloop"        => "The selected :attribute is invalid.",
+	"existsornull"     => "The selected :attribute is invalid.",
+	
 	"image"            => "The :attribute must be an image.",
 	"in"               => "The selected :attribute is invalid.",
 	"integer"          => "The :attribute must be an integer.",
