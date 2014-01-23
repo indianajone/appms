210572175ce04475c6293e26de210ff73d98d12b
Best <mmer555@hotmail.com>
2014-01-23 15:44:46 +0700

    Remove unused

--- workbench/core/plugin/src/models/PluginMethod.php
+++
@@ -1,8 +0,0 @@
-<?php namespace Core\Plugin\Models;
-
-class PluginMethod extends \Illuminate\Database\Eloquent\Model {
-    
-    protected $table = 'plugin_method';
-    protected $primaryKey = "method_id";
-    public $timestamps = false;
-}
--- workbench/kitti/subproject/src/models/PluginInventory.php
+++
@@ -1,7 +0,0 @@
-<?php namespace Kitti\Subproject\Models;
-
-class PluginInventory extends \Illuminate\Database\Eloquent\Model {
-	protected $table = 'plugin_inventory';
-        protected $primaryKey = "plugin_id";
-        public $timestamps = false;
-}
