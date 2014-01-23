9925b22a081e77aa0d8afe316b2e473858045a33
Kitti Klinbua <codetotti@gmail.com>
2014-01-23 18:05:27 +0700

    Revert "Medias beta"
    This reverts commit 1b3fe1ed7d0512d6cafa7653a1d2270f59e81a7a.

--- workbench/kitti/medias/src/Kitti/Medias/Medias.php
+++
@@ -1,9 +0,0 @@
-<?php namespace Kitti\Medias;
-
-class Medias extends \BaseModel
-{
-	protected $table = 'medias';
-	// protected $fillable = array('app_id', 'gallery_id', 'categories_id','pre_title','picture','teaser','content','wrote_by','publish_at','views','tags','status');
-	// protected $guarded = array('id');
-
-}
