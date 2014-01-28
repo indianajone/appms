55752758778f12ca80b780ae14846829021da83e
Best <mmer555@hotmail.com>
2014-01-28 14:45:43 +0700

    Update User Routes

--- workbench/max/user/src/routes.php
+++ workbench/max/user/src/routes.php
@@ -14,15 +14,15 @@
 Route::group(array('prefix' => 'api/v1'), function()
 {
     // Auth
-    Route::any('auth/login', 'Max\\User\\Controllers\\UserController@doLogin');
-    Route::any('auth/logout', 'Max\\User\\Controllers\\UserController@doLogout');
+    Route::any('users/login', 'Max\\User\\Controllers\\UserController@doLogin');
+    Route::any('users/logout', 'Max\\User\\Controllers\\UserController@doLogout');
     
     // Users
     Route::get('users/fields', function(){
         return Response::fields('users');
     });
     Route::post('users/{id}/delete', 'Max\\User\\Controllers\\UserController@delete');
-    Route::post('users/{id}/resetPassword', 'Max\\User\\Controllers\\UserController@resetPassword'); 
+    Route::get('users/{id}/resetPassword', 'Max\\User\\Controllers\\UserController@resetPassword'); 
     Route::post('users/{id}/roles/{action}', 'Max\\User\\Controllers\\UserController@manageRole'); 
     Route::resource('users', 'Max\\User\\Controllers\\UserController');
         
