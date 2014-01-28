8f05c42135decf1a891c4260da7292fb11d1428e
Best <mmer555@hotmail.com>
2014-01-28 14:44:57 +0700

    Edit Member Routes

--- workbench/max/member/src/routes.php
+++ workbench/max/member/src/routes.php
@@ -13,9 +13,15 @@
 
 Route::group(array('prefix' => 'api/v1'), function()
 {
+	 // Auth
+    Route::any('members/login', 'Max\\Member\\Controllers\\MemberController@doLogin');
+    Route::any('members/logout', 'Max\\Member\\Controllers\\MemberController@doLogout');
+
+    // Member
 	Route::get('members/fields', function(){
         return Response::fields('members');
     });
 	Route::post('members/{id}/delete', 'Max\\Member\\Controllers\\MemberController@delete');
+	Route::get('members/{id}/resetPassword', 'Max\\Member\\Controllers\\MemberController@resetPassword'); 
 	Route::resource('members', 'Max\\Member\\Controllers\\MemberController');
 });
