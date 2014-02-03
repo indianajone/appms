2c9fb760d01de3920d7df2a4ba309450ed0254c9
indianajone <mmer555@hotmail.com>
2014-02-03 11:16:53 +0700

    Update Article

--- workbench/kitti/articles/src/Kitti/Articles/Article.php
+++ workbench/kitti/articles/src/Kitti/Articles/Article.php
@@ -4,36 +4,44 @@ class Article extends \BaseModel
 {
     protected $table = 'articles';
     protected $guarded = array('id');
+    protected $hidden = array('app_id', 'status');
 
     public static $rules = array(
     	'show' => array(
     		'appkey' => 'required|exists:applications,appkey'
     	),
+        'show_with_id' => array(
+            'appkey' => 'required|exists:applications,appkey',
+            'id'    => 'required|exists:articles'
+        ),
     	'create' => array(
     		'appkey' => 'required|exists:applications,appkey',
-    		'gallery_id' => 'exists:galleries',
-    		'category_id' => 'existloop:categories,id',
-    		'title' => 'required',
-    		'content' => 'required',
-    		'wrote_by' => 'required'
-    	)
+            'title' => 'required',
+            'content' => 'required',
+            'wrote_by' => 'required',
+    		'category_id' => 'required|existloop:categories,id',
+            'gallery_id' => 'exists:galleries,id'
+    	),
+        'update' => array(
+            'appkey' => 'required|exists:applications,appkey',
+            'id'    => 'required|exists:articles',
+            'category_id' => 'existloop:categories,id',
+            'gallery_id' => 'exists:galleries,id'
+        ),
+        'delete' => array(
+            'appkey' => 'required|exists:applications,appkey',
+            'id'    => 'required|exists:articles'
+        )
     );
 
     public function categories()
     {
-    	return $this->belongsToMany('Indianajone\\Categories\\Category', 'article_category', 'article_id')->withPivot('category_id');
+    	return $this->belongsToMany('Indianajone\\Categories\\Category', 'article_category');
     }
 
-    public function scopeWhereCat($query, $cats)
+    public function gallery()
     {
-        $ids = $this->categories()->wherePivot('category_id', '=', 1);
-        dd($ids->get());
-        dd($query->whereIn('id', $cats)->toSql());
-      // return $query->whereHas('article_category', 
-    	// return $query->wherehas('categories', function($q) use ($cats){
-    	// 	$q->whereIn('article_category.category_id', $cats);
-    	// });
-    	//->whereIn('article_category.category_id', $cats);
+        return $this->hasOne('Kitti\\Galleries\\Gallery', 'content_id')->where('content_type', '=', 'article');
     }
 
     public function attachCategory($category)
@@ -51,5 +59,27 @@ class Article extends \BaseModel
     	foreach ($categories as $category) {
     		$this->attachCategory($category);
     	}
+    }
+
+    public function detachCategory($category)
+    {
+        if(is_object($category))
+            $category = $category->getKey();
+        if(is_array($category))
+            $category = $category['id'];
+
+        $this->categories()->detach($category);
+    }
+
+    public function detachCategories($categories)
+    {
+        foreach ($categories as $category) {
+            $this->detachCategory($category);
+        }
+    }
+
+    public function getCategoryIds()
+    {
+       return  $this->categories()->get(array('category_id'))->toArray();
     }
 }
--- workbench/kitti/articles/src/controllers/ArticleController.php
+++ workbench/kitti/articles/src/controllers/ArticleController.php
@@ -1,13 +1,15 @@
 <?php namespace Kitti\Articles\Controllers;
 
-use \Appl;
-use \BaseController;
-use \Carbon\Carbon;
-use \Input;
-use \Response;
-use \Validator;
-use \Kitti\Articles\Article;
-use \Indianajone\Categories\Category as Categories;
+use App;
+use Appl;
+use BaseController;
+use Carbon\Carbon;
+use Input;
+use Response;
+use Validator;
+use Kitti\Articles\Article;
+use Indianajone\Categories\Category;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class ArticleController extends BaseController
 {
@@ -22,20 +24,26 @@ class ArticleController extends BaseController
 
 		if($validator->passes())
 		{
+			$articles = Article::app()->active();
+
 			$categories = Input::get('category_id', null);
-			if(!$categories)
-				$articles = Article::active()->app()->offset($offset)->limit($limit)->with('categories')->get();
+			if($categories)
+				$articles = $articles->whereHas('categories', function($q)
+				{
+				    $cat = Category::find(Input::get('category_id'))->getDescendantsAndSelf(array('id'));
+				    $q->whereIn('category_id', array_flatten($cat->toArray()));
 
-			else 
-				$articles = Article::whereCat(explode(',',$categories))->get();
+				});
+			
+			$keyword = Input::get('q', null);
+			if($keyword)
+				$articles = $articles->where('title', 'like', '%'.$keyword.'%')->orWhere('content','like', '%'.$keyword.'%');
 
-			// $article = Article::with('categories')->whereCat(array(1))->get();
-
-			// dd(\DB::getQueryLog());
-
+			$articles = $articles->offset($offset)->limit($limit)->get();
+			
 			$articles->each(function($article) use ($fields, $field){
 	 			if($field) $article->setVisible($fields);
-	 			// $article->categories->toHierarchy();
+	 			$article->setRelation('categories', $article->categories()->with('children')->get());
 	 		});
 
 	 		return Response::listing(
@@ -57,11 +65,6 @@ class ArticleController extends BaseController
 
 	public function store()
 	{
-		Validator::resolver(function($translator, $data, $rules, $messages)
-		{
-		    return new \Indianajone\Validators\Rules\ExistLoop($translator, $data, $rules, $messages);
-		});
-
 		$inputs = Input::all();
 		$validator = Validator::make($inputs, Article::$rules['create']);
 
@@ -108,27 +111,113 @@ class ArticleController extends BaseController
 
 	public function show($id)
 	{
-		# code...
+		$inputs = array_add(Input::all(),'id',$id);
+		$validator = Validator::make($inputs, Article::$rules['show_with_id']);
+
+		$field = Input::get('fields', null);
+		$fields = explode(',', $field);
+
+		if($validator->passes())
+		{
+			$article = Article::app()->active()->find($id);
+			if($article)
+			{
+
+				$article->setRelation('categories', $article->categories()->with('children')->first()->getDescendantsAndSelf()->toHierarchy());
+				$article->setRelation('gallery', $article->gallery()->with('medias')->first());
+
+				if($field) $article->setVisible($fields);  
+
+				return Response::result(array(
+	        		'header' => array(
+	        			'code' => 200,
+	        			'message' => 'success'
+	        		),
+	        		'entry' => $article->toArray()
+	        	));
+	        }
+
+	        return Response::message(204, 'Article id:'. $id . ' does not exists.');
+		}
+
+		return Response::message(400, $validator->messages()->first()); 
 	}
 
 	public function edit($id)
 	{
-		$this->update($id);
+		return $this->update($id);
 	}
 
 	public function update($id)
 	{
-		# code...
+		$inputs = array_merge(array('id'=>$id), Input::all());
+		$validator = Validator::make($inputs, Article::$rules['update']);	
+
+		if($validator->passes())
+		{
+			$article = Article::where('id','=',$id)->app()->first();
+
+			$inputs = Input::all();
+			foreach ($inputs as $key => $val) {
+                if( $val == null || 
+                    $val == '' || 
+                    $val == $article[$key] ||
+                    $key == 'appkey' ||
+                    $key == 'id') 
+                {
+                    unset($inputs[$key]);
+                }
+            }
+
+            if(!count($inputs))
+                return Response::message(200, 'Nothing is update.');
+
+            $category_id = Input::get('category_id', null);
+			if($category_id)
+			{
+				$ids = explode(',', $category_id); 
+				$article->attachCategories($ids);
+				unset($inputs['category_id']);
+			}
+
+			$article->update($inputs);
+
+            if($article->save())
+            	return Response::message(200, 'Updated article_id: '.$id.' success!');
+
+            return Response::message(500, 'Something wrong while trying to update.');
+		}
+
+		return Response::message(400, $validator->messages()->first());
 	}
 
 	public function delete($id)
 	{
-		$this->destroy($id);
+		return $this->destroy($id);
 	}
 
 	public function destroy($id)
 	{
-		# code...
+		$inputs = array_add(Input::all(), 'id', $id);
+		$validator = Validator::make($inputs, Article::$rules['delete']);
+
+		if($validator->passes())
+		{
+	        $article = Article::where('id','=',$id)->app()->first();
+	        if($article)
+	        {
+	        	$article->detachCategories(array_flatten($article->getCategoryIds()));	
+				$article->delete();
+
+				return Response::message(200, 'Deleted article_'.$id.' success!'); 
+	        }
+			
+			return Response::message(204, 'article_'.$id.' does not exists!'); 
+		}
+
+		return Response::message(400, $validator->messages()->first());
 	}
+
+
 
 }
